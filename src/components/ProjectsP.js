d0a99a24516ea2a1f962344d1302d58bef36d46b
shinedark <mide0303@gmail.com>
2021-02-25 14:24:02 -0600

    new adds

--- src/components/ProjectsP.js
+++ src/components/ProjectsP.js
@@ -5,48 +5,46 @@ import firebase from "../firebase";
 import "../App.css";
 
 function ProjectsP(props) {
-  const [selectLogin, setSelectLongin] = useState(false);
-  const checkForUser = () => {
-    if (!firebase.getCurrentUsername()) {
-      // not logged in
-      // props.history.replace('/projects')
-      return null;
-    } else {
-      props.history.replace("/blog");
-    }
-  };
-  checkForUser();
+	const [selectLogin, setSelectLongin] = useState(false);
+	const checkForUser = () => {
+		if (!firebase.getCurrentUsername()) {
+			// not logged in
+			// props.history.replace('/projects')
+			return null;
+		} else {
+			props.history.replace("/blog");
+		}
+	};
+	checkForUser();
 
-  const renderAuth = () => {
-    if (selectLogin) {
-      return (
-        <div className="containerAuth">
-          <AuthLogIn />
-          <div onClick={() => setSelectLongin(false)} className="linkAuth">
-            Don't have a account please <strong>sign up</strong>
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div className="containerAuth">
-          <AuthSignUp />
-          <div onClick={() => setSelectLongin(true)} className="linkAuth">
-            Already have a account? plase <strong>log in</strong>
-          </div>
-        </div>
-      );
-    }
-  };
+	const renderAuth = () => {
+		if (selectLogin) {
+			return (
+				<div className="containerAuth">
+					<AuthLogIn />
+					<div onClick={() => setSelectLongin(false)} className="linkAuth">
+						Don't have a account please <strong>sign up</strong>
+					</div>
+				</div>
+			);
+		} else {
+			return (
+				<div className="containerAuth">
+					<AuthSignUp />
+					<div onClick={() => setSelectLongin(true)} className="linkAuth">
+						Already have a account? plase <strong>log in</strong>
+					</div>
+				</div>
+			);
+		}
+	};
 
-  return (
-    <>
-    <h3>Create an account to explore Blog</h3>
-    <div className="containerAuth2">
-      {renderAuth()}
-    </div>
-    </>
-  );
+	return (
+		<div>
+			<h3>Create an account to explore Blog</h3>
+			<div className="containerAuth2">{renderAuth()}</div>
+		</div>
+	);
 }
 
 export default ProjectsP;
