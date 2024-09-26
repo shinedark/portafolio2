01c15b8f99423f8f6c4e34a1a67fb50f81eaa42a
Shine Dark <mide0303@gmail.com>
2024-09-26 04:25:33 -0500

    hide contract component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,71 +1,86 @@
-import React, { useEffect, useRef } from 'react';
-import * as THREE from 'three';
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
-import AsciiArtComponent from './AsciiArtComponent';
-import ProjectCube from './ProjectCube';
-import TechStack from './TechStack';
-import SmartContractComparison from './SmartContractComparison';
-import Interest from './Interest';
-import planetariaRadioImage from './pictures/pr.png'; 
-import sdm from './pictures/sdm.png';
-import mmt from './pictures/mmt.png';
-import './App.css';
+import React, { useEffect, useRef } from 'react'
+import * as THREE from 'three'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
+import AsciiArtComponent from './AsciiArtComponent'
+import ProjectCube from './ProjectCube'
+import TechStack from './TechStack'
+import SmartContractComparison from './SmartContractComparison'
+import Interest from './Interest'
+import planetariaRadioImage from './pictures/pr.png'
+import sdm from './pictures/sdm.png'
+import mmt from './pictures/mmt.png'
+import './App.css'
 
 // Sample project data
 const projects = [
   {
-    title: "PLANETARIA RADIO",
-    description: "Planetaria Radio is a unique platform that combines music streaming with blockchain technology. Users can stream music tracks, and if they have a specific polygon NFT in their wallet, they can upload tracks. Each track is stored as an encrypted file on IPFS, ensuring decentralized and secure storage. Track metadata and ownership are managed through smart contracts on Sepolia blockchain.",
+    title: 'PLANETARIA RADIO',
+    description:
+      'Planetaria Radio is a unique platform that combines music streaming with blockchain technology. Users can stream music tracks, and if they have a specific polygon NFT in their wallet, they can upload tracks. Each track is stored as an encrypted file on IPFS, ensuring decentralized and secure storage. Track metadata and ownership are managed through smart contracts on Sepolia blockchain.',
     features: [
-      "Create and upload music tracks with associated metadata.",
-      "Play and stream music directly from the platform.",
-      "Track play counts and engagement using blockchain technology.",
-      "Decentralized file storage ensures longevity and resistance to censorship.",
-      "NFT integration allows for unique ownership and potential monetization of tracks."
+      'Create and upload music tracks with associated metadata.',
+      'Play and stream music directly from the platform.',
+      'Track play counts and engagement using blockchain technology.',
+      'Decentralized file storage ensures longevity and resistance to censorship.',
+      'NFT integration allows for unique ownership and potential monetization of tracks.',
     ],
     stack: [
-      "React", "GraphQL & Apollo Client", "Polygon", "Styled Components", 
-      "Pinata & IPFS", "Remix", "Ethereum Sepolia", "Solidity", "Hardhat"
+      'React',
+      'GraphQL & Apollo Client',
+      'Polygon',
+      'Styled Components',
+      'Pinata & IPFS',
+      'Remix',
+      'Ethereum Sepolia',
+      'Solidity',
+      'Hardhat',
     ],
-    link: "https://planetaria-rdo.onrender.com/",
-    image: planetariaRadioImage // Use the imported image
+    link: 'https://planetaria-rdo.onrender.com/',
+    image: planetariaRadioImage, // Use the imported image
   },
   {
-    title: "SHINE DARK MUSIC LABEL SITE",
-    description: "CSS AND DESIGN FUN",
-    features: [
-      "FUN FUN FUN"
-    ],
-    stack: ["Next.js"],
-    link: "https://www.shinedarkmusic.com/",
-    image: sdm
+    title: 'SHINE DARK MUSIC LABEL SITE',
+    description: 'CSS AND DESIGN FUN',
+    features: ['FUN FUN FUN'],
+    stack: ['Next.js'],
+    link: 'https://www.shinedarkmusic.com/',
+    image: sdm,
   },
   {
-    title: "MY MED HISTORY ",
-    description: "Creates history to be shared with new doctors or anyone who want's to comprehend more about my health treatment.",
+    title: 'MY MED HISTORY ',
+    description:
+      "Creates history to be shared with new doctors or anyone who want's to comprehend more about my health treatment.",
     features: [
-      "Log medications, supplements, history, informations, share with doctors or family, protected by blockchain technology."
+      'Log medications, supplements, history, informations, share with doctors or family, protected by blockchain technology.',
     ],
-    stack: ["Reatc, Graphql, Web3, "],
-    link: "https://tribe-made-frontend.onrender.com/",
-    image: mmt
+    stack: ['Reatc, Graphql, Web3, '],
+    link: 'https://tribe-made-frontend.onrender.com/',
+    image: mmt,
   },
   // Add more projects as needed
-];
+]
 
 function App() {
-  const headerRef = useRef(null);
+  const headerRef = useRef(null)
 
   useEffect(() => {
     if (headerRef.current) {
-      const scene = new THREE.Scene();
-      const camera = new THREE.PerspectiveCamera(75, headerRef.current.clientWidth / headerRef.current.clientHeight, 0.1, 1000);
-      const renderer = new THREE.WebGLRenderer({ alpha: true });
-      renderer.setSize(headerRef.current.clientWidth, headerRef.current.clientHeight);
-      headerRef.current.appendChild(renderer.domElement);
+      const scene = new THREE.Scene()
+      const camera = new THREE.PerspectiveCamera(
+        75,
+        headerRef.current.clientWidth / headerRef.current.clientHeight,
+        0.1,
+        1000,
+      )
+      const renderer = new THREE.WebGLRenderer({ alpha: true })
+      renderer.setSize(
+        headerRef.current.clientWidth,
+        headerRef.current.clientHeight,
+      )
+      headerRef.current.appendChild(renderer.domElement)
 
-      const loader = new FontLoader();
+      const loader = new FontLoader()
       loader.load('/fonts/helvetiker_regular.typeface.json', (font) => {
         const textGeometry = new TextGeometry('SHINE DARK', {
           font: font,
@@ -76,49 +91,53 @@ function App() {
           bevelThickness: 0.03,
           bevelSize: 0.02,
           bevelOffset: 0,
-          bevelSegments: 5
-        });
-        textGeometry.center();
-        const textMaterial = new THREE.MeshPhongMaterial({ 
+          bevelSegments: 5,
+        })
+        textGeometry.center()
+        const textMaterial = new THREE.MeshPhongMaterial({
           color: 0xffffff,
           specular: 0xffffff,
-          shininess: 100
-        });
-        const textMesh = new THREE.Mesh(textGeometry, textMaterial);
-        scene.add(textMesh);
+          shininess: 100,
+        })
+        const textMesh = new THREE.Mesh(textGeometry, textMaterial)
+        scene.add(textMesh)
 
         // Add lights to the scene
-        const pointLight = new THREE.PointLight(0xffffff, 1, 100);
-        pointLight.position.set(0, 0, 10);
-        scene.add(pointLight);
+        const pointLight = new THREE.PointLight(0xffffff, 1, 100)
+        pointLight.position.set(0, 0, 10)
+        scene.add(pointLight)
 
-        const ambientLight = new THREE.AmbientLight(0x404040); // soft white light
-        scene.add(ambientLight);
+        const ambientLight = new THREE.AmbientLight(0x404040) // soft white light
+        scene.add(ambientLight)
 
-        camera.position.z = 2;
+        camera.position.z = 2
 
         const animate = () => {
-          requestAnimationFrame(animate);
-          textMesh.rotation.y += 0.01;
-          renderer.render(scene, camera);
-        };
-        animate();
-      });
+          requestAnimationFrame(animate)
+          textMesh.rotation.y += 0.01
+          renderer.render(scene, camera)
+        }
+        animate()
+      })
 
       // Handle window resize
       const handleResize = () => {
-        camera.aspect = headerRef.current.clientWidth / headerRef.current.clientHeight;
-        camera.updateProjectionMatrix();
-        renderer.setSize(headerRef.current.clientWidth, headerRef.current.clientHeight);
-      };
-      window.addEventListener('resize', handleResize);
+        camera.aspect =
+          headerRef.current.clientWidth / headerRef.current.clientHeight
+        camera.updateProjectionMatrix()
+        renderer.setSize(
+          headerRef.current.clientWidth,
+          headerRef.current.clientHeight,
+        )
+      }
+      window.addEventListener('resize', handleResize)
 
       return () => {
-        window.removeEventListener('resize', handleResize);
-        headerRef.current.removeChild(renderer.domElement);
-      };
+        window.removeEventListener('resize', handleResize)
+        headerRef.current.removeChild(renderer.domElement)
+      }
     }
-  }, []);
+  }, [])
 
   return (
     <div className="app">
@@ -136,18 +155,22 @@ function App() {
           ))}
         </div>
         <Interest />
-        <SmartContractComparison />
+        {/* <SmartContractComparison /> */}
       </main>
       <footer className="footer">
         <p>© 2024 SHINE DARK. All rights reserved.</p>
         <p>
-          <a href="https://x.com/ShineDarkmusic" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://x.com/ShineDarkmusic"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Contact us on X (Twitter)
           </a>
         </p>
       </footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
